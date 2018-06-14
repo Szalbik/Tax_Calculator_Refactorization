8e32dd5132570c79a4fb3f6bf27b98c483fb65f0
kusmp <patryk.kusmierkiewicz@gmail.com>
2018-06-14 18:16:57 +0200

    add test

---
+++ src/com/bartoszwalter/students/test/ContractTest.java
@@ -0,0 +1,16 @@
+package com.bartoszwalter.students.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractTest {
+
+    @Test
+    void getContract() {
+    }
+
+    @Test
+    void setContract() {
+    }
+}
